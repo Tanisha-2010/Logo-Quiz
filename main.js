e854d031c36000bf365c46a67673bf262d40f21e
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-09-07 19:14:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function page1() {
+    window.location = "logo1.html";
+}
+
+function page2() {
+    window.location = "logo2.html";
+}
+
+function page3() {
+    window.location = "logo3.html";
+}
+
+function page4() {
+    window.location = "logo4.html";
+}
+
+function page5() {
+    window.location = "logo5.html";
+}
+
+function page6() {
+    window.location = "logo6.html";
+}
